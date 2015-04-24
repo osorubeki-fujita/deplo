e64a47697082b81b78a65f1f6abff6885675f4fc
Shu Foujita <osorubeki.fujita@gmail.com>
2015-04-25 08:40:52 +0900

    20150425_0840 Installing 0.1.1

--- lib/deployer/settings.rb
+++ lib/deployer/settings.rb
@@ -1,6 +1,6 @@
 def set_cap_consts
   set :gem , fetch( :application ).gsub( /\Agem_/ , "" )
-  set :latest_version_file , ::File.expand_path( "#{ fetch( :pj_dir ) }/LatestVersion" )
+  set :latest_version_file , ::File.expand_path( "#{ fetch( :pj_dir ) }/.latest_version" )
 
   require "#{ fetch( :pj_dir ) }/lib/#{ fetch( :gem ) }/version.rb"
   set :new_version , eval( "#{ fetch( :gem ).camelize }::VERSION" )
