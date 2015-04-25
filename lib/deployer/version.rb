ca9b3394e6eabca069306075eb833360ca30b179
Shu Foujita <osorubeki.fujita@gmail.com>
2015-04-25 09:51:18 +0900

    20150425_0951 Installing 0.1.4

--- lib/deployer/version.rb
+++ lib/deployer/version.rb
@@ -1,3 +1,3 @@
 module Deployer
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 end
--- spec/deployer_spec.rb
+++ spec/deployer_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
+spec_filename = ::File.expand_path( ::File.dirname( __FILE__ ) )
+version = "0.1.4"
+
 describe Deployer do
   it 'has a version number' do
-    expect(Deployer::VERSION).not_to be nil
+    expect( Deployer::VERSION ).to eq( version )
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it "has module method \"version_check\"." do
+    expect( Deployer.version_check( Deployer::VERSION , spec_filename ) ).to eq( true )
   end
 end
