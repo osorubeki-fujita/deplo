9d2b7ddd674b505f8d394fab4373386a534a154c
Shu Foujita <osorubeki.fujita@gmail.com>
2015-04-27 08:54:33 +0900

    20150427_085400 Install 0.3.3

--- spec/deployer_spec.rb
+++ spec/deployer_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 spec_filename = ::File.expand_path( ::File.dirname( __FILE__ ) )
-version = "0.3.2"
+version = "0.3.3"
 
 describe Deployer do
   it "has a version number \'#{ Deployer::VERSION }\'" do
