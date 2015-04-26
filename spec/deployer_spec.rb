20239ffe30da8baedf356adf49a9741ed338fdd4
Shu Foujita <osorubeki.fujita@gmail.com>
2015-04-27 01:38:34 +0900

    20150427_013800 Installing 0.2.1

--- spec/deployer_spec.rb
+++ spec/deployer_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 spec_filename = ::File.expand_path( ::File.dirname( __FILE__ ) )
-version = "0.2.0"
+version = "0.2.1"
 
 describe Deployer do
   it "has a version number \'#{ Deployer::VERSION }\'" do
