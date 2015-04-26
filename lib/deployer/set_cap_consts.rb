3eb04124c19f7184d2222986a189f9edc874453a
Shu Foujita <osorubeki.fujita@gmail.com>
2015-04-27 01:48:42 +0900

    20150427_014800 Installing 0.2.3

--- lib/deployer/set_cap_consts.rb
+++ lib/deployer/set_cap_consts.rb
@@ -1,9 +1,11 @@
 def set_cap_consts
-  set :gem , fetch( :application ).gsub( /\Agem_/ , "" ).gsub( /::[a-z\d+_]+\Z/ , "" )
+  set :gem , fetch( :application ).gsub( /\Agem_/ , "" )
   set :latest_version_file , ::File.expand_path( "#{ fetch( :pj_dir ) }/.latest_version" )
 
   # Version of Gem
-  require "#{ fetch( :pj_dir ) }/lib/#{ fetch( :gem ) }/version.rb"
+  dirname = [ fetch( :pj_dir ) , :lib , fetch( :gem ).gsub( /::/ , "_" ) ].map( &:to_s ).join( "/" )
+  require "#{ dirname }/version.rb"
+
   set :new_version , eval( "#{ fetch( :gem ).camelize }::VERSION" )
 
   require "#{ ::File.dirname( __FILE__ ) }/version.rb"
--- lib/deployer/version.rb
+++ lib/deployer/version.rb
@@ -1,3 +1,3 @@
 module Deployer
-  VERSION = "0.2.2"
+  VERSION = "0.2.3"
 end
