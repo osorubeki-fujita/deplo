26491ac48c1743c4194b291f05581695caeb6be3
Shu Fujita <osorubeki.fujita@gmail.com>
2015-06-17 06:52:39 +0900

    20150617_065230 Bug fix - setting of opening latest version file

--- lib/deplo/gem.rb
+++ lib/deplo/gem.rb
@@ -20,11 +20,6 @@ def set_cap_namespace_gem
     task :update do
       ::Deplo.process "gem:update" do
         latest_version_file = fetch( :latest_version_file )
-        unless ::File.exist?( latest_version_file )
-          ::File.open( latest_version_file , "r:utf-8" ) do |f|
-            f.print ''
-          end
-        end
         ::File.open( latest_version_file , "w:utf-8" ) do |f|
           f.print fetch( :new_version )
         end
