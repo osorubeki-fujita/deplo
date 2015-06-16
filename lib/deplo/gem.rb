8406b99b15a1858261494945784e465240b0f8a0
Shu Fujita <osorubeki.fujita@gmail.com>
2015-06-17 06:32:50 +0900

    20150617_063250 Update version (to 0.1.5)

--- lib/deplo/gem.rb
+++ lib/deplo/gem.rb
@@ -19,7 +19,11 @@ def set_cap_namespace_gem
     desc "Gem - Update name of latest version"
     task :update do
       ::Deplo.process "gem:update" do
-        ::File.open( fetch( :latest_version_file ) , "w:utf-8" ) do |f|
+        latest_version_file = fetch( :latest_version_file )
+        unless ::File.exist?( latest_version_file )
+          ::File.open( latest_version_file , "r:utf-8" ).close
+        end
+        ::File.open( latest_version_file , "w:utf-8" ) do |f|
           f.print fetch( :new_version )
         end
       end
